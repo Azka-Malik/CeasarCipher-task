5ea207f323d50ba6497b8c1c80762d812ba1b972
Azka-Malik <azkamalik309@gmail.com>
2022-11-06 23:03:09 +0500

    Task Done

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const prompt = require("prompt-sync")({ sigint: true });
 
-// const age = prompt("How old are you? ");
-// console.log(`You are ${age} years old.`);node 
-
 let text='';
 text= prompt("Text : ");
 var k = parseInt(prompt("Key : "));
@@ -17,12 +14,14 @@ for(i=0; i<text.length;i++)
         charCode = 96 + (charCode - 96 + k) % 26;
         //charCode += k % 26;
     }
-    // else if(charCode > 122){
-    //     charCode = (charCode - 122) + 96;
-    // }
     else if (charCode > 64 && charCode < 91){
         charCode = 64 + (charCode - 64 + k) % 26;
     }
+    else if (charCode == 32)
+    {
+        continue;
+    }
+
     en += String.fromCharCode(charCode);
 }
     console.log(`Encrypted Text: ${en}`);
